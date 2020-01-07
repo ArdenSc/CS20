25a8222c29a6a5c8feabbdba4392c79a0d4ab394
Arden Sinclair <arden.sinclair@rbe.sk.ca>
2020-01-06 22:33:22 -0600

    Change code structure to not use bare code Changed my IDE to use 79 as a max line length which made everything worse and practically unreadable but I guess I can't do anything about it

--- choose_your_adventure_Arden_Sinclair.py
+++ choose_your_adventure_Arden_Sinclair.py
@@ -1,18 +1,33 @@
-from random import choice as randchoice
+# CS20 - P4
+# Arden Sinclair
+# Jan. 06, 2020
+# Dungeon adventure game
+
+from random import choice
 from random import randint
 
 
 def fight(hp, enemy, enemy_hp, running_allowed=True):
-    print(f"You are approached by a {randchoice(('fearsome', 'ferocious', 'evil', 'crazy'))} {enemy}, what do you do?")
+    """Allows the player to engage in combat against an enemy.
+    hp is the health of the player at the start of the fight
+    enemy is the name of the enemy to be fought
+    enemy_hp is the health of the enemy at the start of the fight
+    running_allowed allows or disallows the player to run from a fight
+    """
+    print(f"You are approached by a "
+          f"{choice(('fearsome', 'ferocious', 'evil', 'crazy'))} "
+          f"{enemy}, what do you do?")
     while True:
-        choice = input("Attack or run?\n").lower()
-        if choice == "attack":
+        selection = input("Attack or run?\n").lower()
+        if selection == "attack":
             while hp > 0 and enemy_hp > 0:
-                print(f"You have {hp} health, the {enemy} has {enemy_hp} health.")
+                print(f"You have {hp} health, the {enemy} has {enemy_hp}"
+                      f" health.")
                 while True:
                     print("What do you do?")
-                    choice = input("Light attack, heavy attack, or heal?\n").lower()
-                    if choice in ("heavy", "heavy attack"):
+                    selection = input("Light attack, heavy attack, "
+                                      "or heal?\n").lower()
+                    if selection in ("heavy", "heavy attack"):
                         if randint(0, 4) == 0:
                             print("Your heavy attack missed!")
                         else:
@@ -23,13 +38,13 @@ def fight(hp, enemy, enemy_hp, running_allowed=True):
                             enemy_hp = enemy_hp if enemy_hp > 0 else 0
                             print(f"Your heavy attack did {damage} damage!")
                         break
-                    elif choice in ("light", "light attack"):
+                    elif selection in ("light", "light attack"):
                         damage = randint(1, 3)
                         enemy_hp -= damage
                         enemy_hp = enemy_hp if enemy_hp > 0 else 0
                         print(f"Your light attack did {damage} damage!")
                         break
-                    elif choice == "heal":
+                    elif selection == "heal":
                         heal = randint(1, 3)
                         hp += heal
                         print(f"You heal yourself by {heal} health.")
@@ -42,17 +57,22 @@ def fight(hp, enemy, enemy_hp, running_allowed=True):
                     hp = hp if hp > 0 else 0
                     print(f"The {enemy} hit you for {damage} damage!")
                 else:
-                    print(f"The {enemy} jumped forward and tried to hit you, but fell to the ground.")
+                    print(f"The {enemy} jumped forward and tried to hit you, "
+                          f"but fell to the ground.")
             if hp == 0:
-                print(f"You died to {'an' if enemy[0].lower() in ['a', 'e', 'i', 'o', 'u'] else 'a'} {enemy}, really?")
+                a_or_an = 'an' if enemy[0].lower() in \
+                                  ['a', 'e', 'i', 'o', 'u'] else 'a'
+                print(f"You died to {a_or_an} {enemy}, really?")
                 print("Game Over.")
-                exit()
+                return hp
             elif enemy_hp == 0:
-                print(f"You have slain the {enemy}, well done! You have {hp} health.")
+                print(f"You have slain the {enemy}, well done! You have {hp}"
+                      f" health.")
             break
-        elif choice == "run":
+        elif selection == "run":
             if not running_allowed:
-                print(f"You can't run from the {enemy}, there's nowhere to go!")
+                print(f"You can't run from the {enemy}, "
+                      f"there's nowhere to go!")
                 continue
             if randint(0, 1) == 0:
                 print("You ran away.")
@@ -60,98 +80,170 @@ def fight(hp, enemy, enemy_hp, running_allowed=True):
                 damage = randint(1, 5)
                 hp -= damage
                 hp = hp if hp > 0 else 0
-                print(f"The {enemy} struck you from behind, dealing {damage} damage!")
+                print(f"The {enemy} struck you from behind, dealing {damage}"
+                      f" damage!")
                 if hp == 0:
                     print("You died!")
                     print("Game Over.")
-                    exit()
-                print(f"You ran away, wounded from the slash with {hp} health remaining.")
+                    return hp
+                print(f"You ran away, wounded from the slash with {hp}"
+                      f" health remaining.")
             break
         else:
             print("Invalid choice, try again.")
     return hp
 
 
-hp = 20
-print("Welcome to the dungeon adventure!")
-while True:
-    print("You are confronted with a cave opening. Will you go in and see what's inside?")
-    choice = input("Enter the cave or run away?\n").lower()
-    if choice in ("enter the cave", "enter", "go in", "cave"):
-        print("You follow the tunnel, holding your torch out in front of you for light.\n"
-              "Something runs quickly past your side. A sense of danger looms.\n"
-              "It runs back towards you. You can now see that it's a massive rat!")
-        if randint(0, 1) == 0:
-            print("The rat squeaks and runs behind you. It didn't want to bite such a fearsome foe.")
+def game(hp):
+    """Contains the story and choices of the adventure game
+    hp is the player's starting health
+    """
+    print("Welcome to the dungeon adventure!")
+    while True:
+        print("You are confronted with a cave opening. Will you go in and "
+              "see what's inside?")
+        selection = input("Enter the cave or run away?\n").lower()
+        if selection in ("enter the cave", "enter", "go in", "cave"):
+            print("You follow the tunnel, holding your torch out in front of "
+                  "you for light.\n"
+                  "Something runs quickly past your side. A sense of danger "
+                  "looms.\n"
+                  "It runs back towards you. You can now see that it's a "
+                  "massive rat!")
+            if randint(0, 1) == 0:
+                print("The rat squeaks and runs behind you. It didn't want "
+                      "to bite such a fearsome foe.")
+            else:
+                hp = fight(hp, "Rat", 4)
+                if hp == 0:
+                    return
+            print("You continue on, finding a skeleton leaned against the "
+                  "wall.\n"
+                  "One of its bones looks weird and shiny. What do you do?")
+            while True:
+                selection = input("Grab the bone or leave it?\n").lower()
+                if selection in ("grab the bone", "grab it", "grab"):
+                    print("The wall behind you creaks and splits, revealing "
+                          "a secret passage!\n"
+                          "There is a skeleton guarding the passage with a "
+                          "sword and shield!")
+                    hp = fight(hp, "Skeleton", 15)
+                    if hp == 0:
+                        return
+                    print("With the skeleton gone, you trudge on, "
+                          "encountering\n"
+                          "a fork in the path. Where do you go?")
+                    while True:
+                        selection = input("Left or right?\n").lower()
+                        if selection == "left":
+                            print("You walk through the left passage. A "
+                                  "metal door falls from above,\n"
+                                  "blocking the way back. You hear a massive "
+                                  "RRRRRROOOOOAAAAARRRRR!\n"
+                                  "A giant Ogre appears out of the smoke in "
+                                  "front of you!")
+                            hp = fight(hp, "Ogre", 50, False)
+                            if hp == 0:
+                                return
+                            print("With his dying breath, the Ogre pulls out "
+                                  "a key from his pocket.\n"
+                                  "You see a door behind him with a keyhole "
+                                  "about the same size as the key.\n"
+                                  "You can try the key and see what's "
+                                  "behind. What do you do?")
+                            while True:
+                                selection = input("Try the key or "
+                                                  "run away?\n").lower()
+                                if selection in ("try the key", "key"):
+                                    print("The key fits and turns, revealing "
+                                          "a room full of gold and gems!\n"
+                                          "Congratulations, you win!")
+                                    return
+                                elif selection in ("run away", "run"):
+                                    print("You run away from the door in "
+                                          "fear of a trap. You find yourself "
+                                          "back\n"
+                                          "at the start of the cave.")
+                                    break
+                                else:
+                                    print("Invalid choice, try again.")
+                            break
+                        elif selection == "right":
+                            print("You go through the right passage. From "
+                                  "all around you, different foes appear!")
+                            hp = fight(hp, "Zombie", 5, False)
+                            if hp == 0:
+                                return
+                            hp = fight(hp, "Poisonous Bat", 10, False)
+                            if hp == 0:
+                                return
+                            hp = fight(hp, "Troll", 20, False)
+                            if hp == 0:
+                                return
+                            print("With the trio dealt with, you find a key "
+                                  "lying on the ground beside them.\n"
+                                  "You see a door behind with a keyhole "
+                                  "about the same size as the key.\n"
+                                  "You can try the key and see what's "
+                                  "behind. What do you do?")
+                            while True:
+                                selection = input("Try the key or "
+                                                  "run away?\n").lower()
+                                if selection in ("try the key", "key",
+                                                 "try it"):
+                                    print("The key fits and turns, revealing "
+                                          "a room full of gold and gems!\n"
+                                          "Congratulations, you win!")
+                                    return
+                                elif selection in ("run away", "run"):
+                                    print("You run away from the door in "
+                                          "fear of a trap. You find yourself "
+                                          "back\n"
+                                          "at the start of the cave.")
+                                    break
+                                else:
+                                    print("Invalid choice, try again.")
+                            break
+                        else:
+                            print("Invalid choice, try again.")
+                    break
+                elif selection in ("leave it", "leave"):
+                    print("You ignore the skeleton, walking further into the "
+                          "tunnel.\n"
+                          "The tunnel opens up into a room full of lava and "
+                          "spikes. Looks like this wasn't\n"
+                          "the path you wanted. You turn back and leave, "
+                          "soon finding yourself at the start\n"
+                          "of the cave.")
+                    break
+                else:
+                    print("Invalid choice, try again.")
+        elif selection in ("run away", "run"):
+            print("You run away from the cave, too scared of what awaits you "
+                  "inside.")
+            print("Game over.")
+            return
         else:
-            hp = fight(hp, "Rat", 4)
-        print("You continue on, finding a skeleton leaned against the wall.\n"
-              "One of its bones looks weird and shiny. What do you do?")
+            print("Invalid choice, try again.")
+
+
+def main():
+    """Calls the function to play the game and requests if the player wants
+    to play again """
+    while True:
+        # Starts the game and gives the player a starting health of 20
+        game(20)
         while True:
-            choice = input("Grab the bone or leave it?\n").lower()
-            if choice in ("grab the bone", "grab it", "grab"):
-                print("The wall behind you creaks and splits, revealing a secret passage!\n"
-                      "There is a skeleton guarding the passage with a sword and shield!")
-                hp = fight(hp, "Skeleton", 15)
-                print("With the skeleton gone, you trudge on, encountering\n"
-                      "a fork in the path. Where do you go?")
-                while True:
-                    choice = input("Left or right?\n").lower()
-                    if choice == "left":
-                        print("You walk through the left passage. A metal door falls from above,\n"
-                              "blocking the way back. You hear a massive RRRRRROOOOOAAAAARRRRR!\n"
-                              "A giant Ogre appears out of the smoke in front of you!")
-                        hp = fight(hp, "Ogre", 50, False)
-                        print("With his dying breath, the Ogre pulls out a key from his pocket.\n"
-                              "You see a door behind him with a keyhole about the same size as the key.\n"
-                              "You can try the key and see what's behind. What do you do?")
-                        while True:
-                            choice = input("Try the key or run away?\n").lower()
-                            if choice in ("try the key", "key"):
-                                print("The key fits and turns, revealing a room full of gold and gems!\n"
-                                      "Congratulations, you win!")
-                                exit()
-                            elif choice in ("run away", "run"):
-                                print("You run away from the door in fear of a trap. You find yourself back\n"
-                                      "at the start of the cave.")
-                                break
-                            else:
-                                print("Invalid choice, try again.")
-                        break
-                    elif choice == "right":
-                        print("You go through the right passage. From all around you, different foes appear!")
-                        hp = fight(hp, "Zombie", 5, False)
-                        hp = fight(hp, "Poisonous Bat", 10, False)
-                        hp = fight(hp, "Troll", 20, False)
-                        print("With the trio dealt with, you find a key lying on the ground beside them.\n"
-                              "You see a door behind with a keyhole about the same size as the key.\n"
-                              "You can try the key and see what's behind. What do you do?")
-                        while True:
-                            choice = input("Try the key or run away?\n").lower()
-                            if choice in ("try the key", "key"):
-                                print("The key fits and turns, revealing a room full of gold and gems!\n"
-                                      "Congratulations, you win!")
-                                exit()
-                            elif choice in ("run away", "run"):
-                                print("You run away from the door in fear of a trap. You find yourself back\n"
-                                      "at the start of the cave.")
-                                break
-                            else:
-                                print("Invalid choice, try again.")
-                        break
-                    else:
-                        print("Invalid choice, try again.")
-            elif choice in ("leave it", "leave"):
-                print("You ignore the skeleton, walking further into the tunnel.\n"
-                      "The tunnel opens up into a room full of lava and spikes. Looks like this wasn't\n"
-                      "the path you wanted. You turn back and leave, soon finding yourself at the start\n"
-                      "of the cave.")
+            selection = input("Would you like to play again? Yes or no?\n"). \
+                lower()
+            if selection == "yes":
+                print("Sure. Lets play another round, shall we?")
                 break
+            elif selection == "no":
+                print("Alright. Hope you had fun!")
+                exit()
             else:
                 print("Invalid choice, try again.")
-    elif choice in ("run away", "run"):
-        print("You run away from the cave, too scared of what awaits you inside.")
-        print("Game over.")
-        exit()
-    else:
-        print("Invalid choice, try again.")
+
+
+main()
