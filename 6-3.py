b026b06e1edf65d34d96210be4c4e782bb209f33
ArdenSinclair <54864498+ArdenSinclair@users.noreply.github.com>
2020-01-15 13:32:37 -0600

    Added 6-3

---
+++ 6-3.py
@@ -0,0 +1,13 @@
+glossary = {
+    'list': 'an ordered data structure holding a sequence of items',
+    'dictionary': 'a data structure that has key and value pairs',
+    'function': 'a reusable block of code that performs an action and '
+                'returns a result',
+    'abstraction': 'a strategic removal of details to simplify the process '
+                   'for achieving a goal',
+    'algorithm': 'an order of actions for performing a task or solving a '
+                 'problem'
+}
+
+for k, v in glossary.items():
+    print(f"{k.title()}: {v}.")
