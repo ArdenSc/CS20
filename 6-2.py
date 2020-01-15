f2cc6858968c326809865ff1ad66efa3bfbd3c51
ArdenSinclair <54864498+ArdenSinclair@users.noreply.github.com>
2020-01-15 13:30:48 -0600

    Added 6-2

---
+++ 6-2.py
@@ -0,0 +1,10 @@
+favorite_numbers = {
+    'Arden': 1,
+    'Aunglay': 4,
+    'Huy': 21,
+    'Evan': 2,
+    'Ethan': 7
+}
+
+for k, v in favorite_numbers.items():
+    print(f"{k}'s favorite number is {v}")
